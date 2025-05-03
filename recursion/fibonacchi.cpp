b98ef5bd88e1bc65267be518266966e7b86ceb56
Hasib Muhammad <hasibmu99@gmail.com>
2025-05-03 13:31:16 +0000

    added fibonacchi series number uisng recursion

---
+++ recursion/fibonacchi.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fib(int n) {
+    if(n <= 1) {
+        return n;
+    }
+    int last = fib(n-1);
+    int slast = fib(n-2);
+
+    return last + slast;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << fib(n) << endl;
+}
